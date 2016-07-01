72d7f09760311485dd50243630ee70bf64758a06
arunabaijal <aruna11baijal@gmail.com>
2016-07-01 13:39:31 +0530

    first commit

---
+++ backend test/test/public/javascripts/angularApp.js
@@ -0,0 +1,6 @@
+(function(app) {
+  document.addEventListener('DOMContentLoaded', function() {
+    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+    ng.platformBrowserDynamic.bootstrap(app.AppComponent2);
+  });
+})(window.app || (window.app = {}));

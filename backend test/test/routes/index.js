5e0ffdfabcc9cec090ac968704f2f55ff34435ba
arunabaijal <aruna11baijal@gmail.com>
2016-07-01 23:12:04 +0530

    Update index.js

--- backend test/test/routes/index.js
+++ backend test/test/routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var name = mongoose.model('name');
+var active = mongoose.model('active');
+var hostingtype=mongoose.model('hostingtype');
+var industry=mongoose.model('industry');
+var backend=mongoose.model('backend');
+var geography=mongoose.model('geography');
+var region=mongoose.model('region');
+var edi=mongoose.model('edi');
+var location=mongoose.model('location');
+var solution=mongoose.model('solution');
+var rtc=mongoose.model('rtc');
+var lifecyle=mongoose.model('lifecyle');
+var fromdate=mongoose.model('from');
+var todate=mongoose.model('todate');
+var thirdparty=mongoose.model('thirdparty');
+var icn=mongoose.model('icn');
+
+router.post('/posts', function(req, res, next) {
+  var name = new name(req.body);
+
+  name.save(function(err, name){
+    if(err){ return next(err); }
+
+    res.json(name);
+  });
+});
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
 module.exports = router;
